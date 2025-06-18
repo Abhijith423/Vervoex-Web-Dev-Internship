8c465c377d157f0ed3c7538a60f06b1ce197ade8
Abhijith <abhijithemes@gmail.com>
2025-06-18 15:13:10 +0530

    script.js

---
+++ script.js
@@ -0,0 +1,65 @@
+document.write("<h1 style='color: red; background-color: green;'>hello world</h1>");
+var a=10;
+var b=20;
+document.write(a+b);
+document.write("<br>");
+var e="Rehan";
+document.writeln(e);               // basics of js
+document.write("<br>");
+var ab=true;
+document.write(ab);
+document.write("<br>");
+var h=40;
+var i="Rehan";
+document.write(typeof h);
+document.write("<br>");
+document.write(typeof i);
+const pi=3.14;
+document.write(pi);
+document.write("<br>")
+var jk=8;
+document.write("<br>");
+var hr=10;
+document.write("<br>");
+var res=jk*hr;
+document.write(res);
+document.write("<br>");
+var gd=12;
+gd++;
+document.write(gd);
+document.write("<br>");
+gd--;
+document.write(gd);
+document.write("<br>");
+var gh=12;
+var sq=gh*gh;
+document.write(sq);
+document.write("<br>");
+var gh=10;
+sq=gh*10;
+document.write(sq);
+var cv=10;
+cv+=3;
+cv*=5;
+document.write("<br>");
+document.write(cv);
+var f=10;
+f%=2; //f=f%2
+document.write("<br>");
+document.write(f);
+//comparison operator
+var g=10;
+var h=20;
+document.write("<br>");
+document.write(g==h);
+document.write("<br>");
+var s=45;
+var t="45";
+document.write(s==t);
+document.write("<br>");
+
+
+
+
+
+
